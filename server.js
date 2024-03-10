1151df091a7ab1e8fb3f2a79f214c405809b49a5
Shree1805yadav <nainay1805@gmail.com>
2024-03-10 22:08:41 +0530

    Merge changes from remote repository

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./index.js";
-
-const PORT = process.env.PORT || 8000;
-
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-  console.log(`Server running on: http://localhost:${PORT}`);
-});
