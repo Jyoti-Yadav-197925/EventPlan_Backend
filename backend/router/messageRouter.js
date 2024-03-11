c3da6e0b865495c98364b06d288c45373d5ee690
Shree1805yadav <nainay1805@gmail.com>
2024-03-12 01:10:56 +0530

    Navbar and Home section complete

--- backend/router/messageRouter.js
+++
@@ -1,8 +0,0 @@
-import { messageSent } from "../controllers/messageControllers.js"; // Importing messageSent controller
-import express from "express";
-
-const router = express.Router();// Create instance of Express router
-
-router.post("/send", messageSent); // POST route for handling the sending messages
-
-export default router;
